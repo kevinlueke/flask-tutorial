3b1354fa47dde93838912bd70b9c56a3669d5cd4
Kevin Lueke <kevinlueke66@gmail.com>
2020-02-19 16:14:39 -0500

    Fix bug
    line 10 in db.py, changed from sqlite to sqlite3

--- flaskr/db.py
+++ flaskr/db.py
@@ -7,7 +7,7 @@ from flask.cli import with_appcontext
 
 def get_db():
     if 'db' not in g:
-        g.db = sqlite.connect(
+        g.db = sqlite3.connect(
                 current_app.config['DATABASE'],
                 detect_types=sqlite3.PARSE_DECLTYPES
         )
